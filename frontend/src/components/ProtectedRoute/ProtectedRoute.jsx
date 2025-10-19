6a76ace9d28a2d8da1606ceffbc4bfabc333470e
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2025-10-20 03:21:48 +0530

    "update"

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -6,6 +6,7 @@ import Cookies from 'js-cookie';
 import api from '@/api/api';
 import { useDispatch } from 'react-redux';
 import { addUser } from '@/features/userDetail/userDetailsSlice';
+import { InstagramSkeletonComponent } from '../Home/instagram-skeleton';
 
 const ProtectedRoute = ({ children }) => {
   const [checking, setChecking] = useState(true);
@@ -49,14 +50,16 @@ const ProtectedRoute = ({ children }) => {
 
   if (checking) {
     // show a loader while validating
-    return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
+    // return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
+    return <InstagramSkeletonComponent />;
   }
-
+  
   if (!allowed) {
     return <Navigate to="/login" replace />;
   }
-
+  
   return children;
 };
 
 export default ProtectedRoute;
+
